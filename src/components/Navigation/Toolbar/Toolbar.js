d17596898ec796bbeba860d0f5fe782291188b5c
the-markussohn <markus.justinas@gmail.com>
2018-09-02 01:13:33 +0300

    dummy navbar

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import css from './Toolbar.css';
+
+const toolbar = (props) => {
+    return (
+        <header className={css.Toolbar}>
+            <div>MENU</div>
+            <div>LOGO</div>
+            <nav>
+                ...
+            </nav>
+        </header>
+    );
+};
+
+export default toolbar;
