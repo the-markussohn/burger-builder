28089a1656a19170bb342bbc40b9fb8048e594e7
the-markussohn <markus.justinas@gmail.com>
2018-09-01 17:56:48 +0300

    build controls in ui

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import css from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'}
+];
 
 const buildControls = (props) => {
     return (
-        <div>
-
+        <div className={css.BuildControls}>
+            {controls.map(control => (
+                <BuildControl key={control.label} label={control.label}/>
+            ))}
         </div>
     );
 };
