87439f4da5632d0989c256aecd7594c02f53db81
the-markussohn <markus.justinas@gmail.com>
2018-09-01 17:52:12 +0300

    single build control

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import css from './BuildControl.css';
+
+const buildControl = (props) => {
+    return (
+        <div className={css.BuildControl}>
+            <div className={css.Label}>{props.label}</div>
+            <button className={css.Less}>Less</button>
+            <button className={css.More}>More</button>
+        </div>
+    );
+};
+
+export default buildControl;
