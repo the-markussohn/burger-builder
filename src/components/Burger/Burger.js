304906a5c34a91abfe1b66819ba92676172dc17f
the-markussohn <markus.justinas@gmail.com>
2018-09-01 17:41:57 +0300

    render no burger ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,19 @@ import css from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(key => {
             return [...Array(props.ingredients[key])]
                 .map((_, index) => {
                     return <BurgerIngredient key={key + index} type={key}/>;
                 });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+    if (transformedIngredients.length === 0) {
+       transformedIngredients = <p>Please start adding ingredients</p>
+    }
     return (
         <div className={css.Burger}>
             <BurgerIngredient type={'bread-top'}/>
