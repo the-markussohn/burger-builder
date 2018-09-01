853c290b5eb47b51bb5bdafa9bb09d3ed8eadeea
the-markussohn <markus.justinas@gmail.com>
2018-09-01 17:34:20 +0300

    output a list of ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import css from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(key => {
+            return [...Array(props.ingredients[key])]
+                .map((_, index) => {
+                    return <BurgerIngredient key={key + index} type={key}/>;
+                });
+        });
     return (
         <div className={css.Burger}>
             <BurgerIngredient type={'bread-top'}/>
-            <BurgerIngredient type={'cheese'}/>
-            <BurgerIngredient type={'meat'}/>
+            {transformedIngredients}
             <BurgerIngredient type={'bread-bottom'}/>
         </div>
     );
