7a0132f2d3fa7a760b023b0ef4f5a1b2db84437e
the-markussohn <markus.justinas@gmail.com>
2018-09-02 16:07:27 +0300

    close side drawer on backdrop click

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,27 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import css from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
+    let attachedClasses = [css.SideDrawer, css.Close];
+    if (props.open) {
+        attachedClasses = [css.SideDrawer, css.Open];
+    }
+
     return (
-        <div className={css.SideDrawer}>
-            <div className={css.Logo}>
-                <Logo/>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={css.Logo}>
+                    <Logo/>
+                </div>
+                <nav>
+                    <NavigationItems/>
+                </nav>
             </div>
-            <nav>
-                <NavigationItems/>
-            </nav>
-        </div>
+        </Aux>
     );
 };
 
