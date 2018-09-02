c86b4da09f5330a01dcc0d227ad951d365348d73
the-markussohn <markus.justinas@gmail.com>
2018-09-02 21:01:09 +0300

    folder restructure

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import css from './Modal.css';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
@@ -8,7 +8,7 @@ class Modal extends Component {
     shouldComponentUpdate(nextProps, nextState) {
         return nextProps.show !== this.props.show;
     }
-    
+
     render() {
         return (
             <Aux>
